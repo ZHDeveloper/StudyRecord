333e024f900f25c06108df963b9c4bb6c5a6cc87
ZhiHua Shen <zhcoder2011@gmail.com>
2018-05-03 13:44:02 +0800

    保存代码

--- PhotoGroupView/PhotoGroupView/ViewController.m
+++ PhotoGroupView/PhotoGroupView/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "YYPhotoGroupView.h"
 
 @interface ViewController ()
 
@@ -17,7 +18,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+}
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    YYPhotoGroupItem *item = [[YYPhotoGroupItem alloc] init];
+    item.largeImageURL = [NSURL URLWithString:@"http://pic.58pic.com/58pic/12/96/26/79W58PICYKg.jpg"];
+    
+    YYPhotoGroupView *groupView = [[YYPhotoGroupView alloc] initWithGroupItems:@[item]];
+    
+    [groupView presentFromImageView:nil toContainer:self.view animated:YES completion:nil];
 }
 
 
